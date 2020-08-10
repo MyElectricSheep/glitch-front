3d01769594af1a68adbab777316b2778315246a7
Ben Paget <ben.paget@live.fr>
2020-08-10 20:11:35 +0100

    feature/useWindowSize + responsiveness

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -13,14 +13,14 @@ const AppBody = (props) => {
         <div className="container-ghost">
           <Ghost size={240} mood={ghostMood} color="#E0E4E8" />
         </div>
-        <section class="fog">
+        <section className="fog">
         <figure
-          class="absolute-bg"
+          className="absolute-bg"
           style={{ backgroundImage: `url(${Background})` }}
         ></figure>
-        <div class="fog__container">
-          <div class="fog__img fog__img--first"></div>
-          <div class="fog__img fog__img--second"></div>
+        <div className="fog__container">
+          <div className="fog__img fog__img--first"></div>
+          <div className="fog__img fog__img--second"></div>
         </div>
       </section>
       </div>
--- src/components/BubbleContent/ChooseChallenge.js
+++ src/components/BubbleContent/ChooseChallenge.js
@@ -2,98 +2,123 @@ import React from "react";
 import { Box, Distribution, Text } from "grommet";
 import styles from "./ChooseChallenge.module.css";
 import SvgIcon from "../../utils/SvgIcon";
+import useWindowSize from "../../utils/useWindowSize";
+
+const ChallengeBox = ({ value }) => {
+  const { width } = useWindowSize();
+
+  return (
+    <Box
+      className={styles.challengeBox}
+      pad={width < 980 ? "medium" : "xsmall"}
+      background={value.color}
+      fill
+      justify="center"
+      align="center"
+      alignContent="center"
+      wrap
+      basis="auto"
+      onClick={() => alert("wesh")}
+    >
+      {value.icon}
+      <Text size="large">{value.title}</Text>
+    </Box>
+  );
+};
 
 const ChooseChallenge = () => {
+  const { width } = useWindowSize();
+
   const possibleChallenges = [
     {
-      value: 50,
+      value: 55,
       title: "HTML5",
       color: "light-3",
       icon: <SvgIcon name="html5" />,
     },
+    {
+        value: 50,
+        title: "JavaScript",
+        color: "light-3",
+        icon: <SvgIcon name="javascript" />,
+      },
+      {
+        value: 45,
+        title: "NodeJS",
+        color: "light-3",
+        icon: <SvgIcon name="nodejs" />,
+      },
     {
       value: 30,
       title: "CSS3",
       color: "light-3",
       icon: <SvgIcon name="css3" />,
     },
+
     {
-      value: 50,
-      title: "JavaScript",
-      color: "light-3",
-      icon: <SvgIcon name="javascript" />,
-    },
-    {
-      value: 30,
+      value: 25,
       title: "React",
       color: "light-3",
       icon: <SvgIcon name="react" />,
     },
+
     {
-      value: 50,
-      title: "NodeJS",
-      color: "light-3",
-      icon: <SvgIcon name="nodejs" />,
-    },
-    {
-      value: 30,
+      value: 20,
       title: "PostgreSQL",
       color: "light-3",
       icon: <SvgIcon name="postgresql" />,
     },
     {
-      value: 30,
+      value: 15,
       title: "MongoDB",
       color: "light-3",
       icon: <SvgIcon name="mongodb" />,
     },
     {
-      value: 30,
+      value: 10,
       title: "Python",
       color: "light-3",
       icon: <SvgIcon name="python" />,
     },
-    {
-      value: 30,
-      title: "MongoDB",
-      color: "light-3",
-      icon: <SvgIcon name="mongodb" />,
-    },
-    {
-      value: 30,
-      title: "Python",
-      color: "light-3",
-      icon: <SvgIcon name="python" />,
-    },
+    // {
+    //   value: 30,
+    //   title: "MongoDB",
+    //   color: "light-3",
+    //   icon: <SvgIcon name="mongodb" />,
+    // },
+    // {
+    //   value: 30,
+    //   title: "Python",
+    //   color: "light-3",
+    //   icon: <SvgIcon name="python" />,
+    // },
     // { value: 10, title: "Angular", color: "light-3", icon:<SvgIcon name="angular" /> },
   ];
   return (
     <div className={styles.flexContainer}>
       <p>Choose your challenge:</p>
-      <Distribution
-        fill
-        basis="full"
-        alignSelf="center"
-        values={possibleChallenges}
-      >
-        {(value) => (
-          <Box
-            className={styles.challengeBox}
-            pad="xsmall"
-            background={value.color}
-            fill
-            justify="center"
-            align="center"
-            wrap
-            // overflow="hidden"
-            onClick={() => alert('wesh')}
-          >
-            {value.icon}
-            <Text size="large">{value.title}</Text>
-            
-          </Box>
-        )}
-      </Distribution>
+      {width < 980 ? (
+        <Box
+          direction="column"
+          //   border={{ color: 'brand', size: 'large' }}
+          pad="medium"
+        >
+          {possibleChallenges.map((value) => {
+            return <ChallengeBox key={`${value.title}-mobile`} value={value} />;
+          })}
+        </Box>
+      ) : (
+        <Distribution
+          fill
+          basis="full"
+          alignSelf="center"
+          values={possibleChallenges}
+        >
+          {(value) => (
+            <ChallengeBox key={`${value.title}-desktop`} value={value} />
+          )}
+        </Distribution>
+      )}
     </div>
   );
 };
